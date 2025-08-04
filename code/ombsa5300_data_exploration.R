873a89cf07572fb0faffca1f488f84eceec0be50
twiz206 <tnguyen65@seattleu.edu>
2025-08-03 22:51:04 -0700

    Processing data

--- code/ombsa5300_data_exploration.R
+++ code/ombsa5300_data_exploration.R
@@ -5,8 +5,25 @@ library(tidyverse)
 library(rio)
 library(lubridate)
 
+
+# Loading data ------------------------------------------------------------
+
 #Read in Google trends files
-trends_files <- list.files(path = "rawdata", pattern = "data\trends_up_to_.*\\.csv", full.names = TRUE)
+trends_files <- list.files(path = "data", pattern = "trends_up_to_.*\\.csv", full.names = TRUE)
 
 #Import and bind together
 trends_raw <- import_list(trends_files, rbind = TRUE)
+
+
+# Processing data ---------------------------------------------------------
+
+# Process the combined trends data
+trends_processed <- trends_raw %>%
+  # Create a proper date variable. The date is the start of the week.
+  mutate(date = ymd(str_sub(monthorweek, 1, 10))) %>%
+  # The Google Trends index is not comparable across keywords. We standardize it
+  # by creating a z-score (subtract mean, divide by standard deviation).
+  group_by(schname, keyword) %>%
+  mutate(index_std = (index - mean(index, na.rm = TRUE)) / sd(index, na.rm = TRUE)) %>%
+  ungroup() %>%
+ 
